75296b75c57c28bdf608961b96a23dec17077acc
Rokas <icestar6215@gmail.com>
2025-03-15 10:59:39 +0800

    feat: add gallery page

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"36335019a8eab588c3c2ea783c618d90505be2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1762266329"
+    serviceWorkerVersion: "3641780170"
   }
 });
